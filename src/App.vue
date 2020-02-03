238e247354377501aa76d3f9dd48d561d124cbc8
Sampletexttt <44058078+Sampletexttt@users.noreply.github.com>
2020-02-03 13:54:16 +0100

    Console log rule
    De console logs worden niet geacepteerd en tijdens dit fixen is de vue server ermee gestopt...

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,41 @@
 <template>
   <div id="app">
+    <Header />
+    <AddGame v-on:add-Game="addGame" />
     <Games v-bind:games="games" v-on:del-game="deleteGame" />
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header'
 import Games from './components/Games';
+import AddGame from './components/AddGame';
+import axios from 'axios';
 
 export default {
   name: "app",
   components: {
-    Games
+    Header,
+    Games,
+    AddGame
   },
   data() {
     return{
-      games: [
-        {
-          id: 1,
-          title: "Game One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Game Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Game Three",
-          completed: false
-        }
-      ]
+      games: []
     }
   },
   methods: {
     deleteGame(id) {
       this.games = this.games.filter(game => game.id !== id);
+    },
+    addGame(newGame) {
+      this.games = [...this.games, newGame];
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(res => this.games = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
@@ -51,5 +50,18 @@ export default {
 body{
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
---
+++ src/components/AddGame.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form @submit="addGame">
+      <input type="text" v-model="title" name="title" placeholder="Add Game...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+  import uuid from 'uuid';
+  export default {
+    name: "AddGame",
+    data() {
+      return {
+        title: ''
+      }
+    },
+    methods: {
+      addGame(e) {
+        e.preventDefault();
+        const newGame = {
+          id: uuid.v4(),
+          title: this.title,
+          completed: false
+        }
+        // send up to parent
+        this.$emit('add-Game', newGame);
+
+        this.title = '';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
